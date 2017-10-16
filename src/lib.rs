fc0b40cb998c337b1c1ad4718419337b1d2f3a18
ahuff44 <andrewhuff4@gmail.com>
2017-10-16 02:56:21 -0600

    trying to add slice iterator; lifetime troubles

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,72 @@ pub mod ttt {
     vals: Vec<Vec<Mark>>,
   }
 
+  struct SliceIter<'a> {
+    board: &'a Board,
+    sr: usize,
+    sc: usize,
+    dr: usize,
+    dc: usize,
+  }
+
+  impl SliceIter {
+    fn new<'a>(&board: Board<'a>) -> SliceIter<'a> {
+      SliceIter{
+        board,
+        sr: 0, sc: 0,
+        dr: 0, dc: 1,
+      }
+    }
+
+    fn advance(&mut self) -> bool {
+      match (self.dr, self.dc) {
+        (0, 1) => {self.dr = 1},
+        (1, 1) => {self.dc = 0},
+        (1, 0) => {self.dc = -1},
+        (1, -1) => {
+          self.dr = 0;
+          self.dc = 1;
+
+          self.sc += 1;
+          if self.sc >= self.board.size {
+            self.sc = 0;
+            self.sr += 1;
+            if self.sr >= self.board.size {
+              return false;
+            }
+          }
+        },
+      }
+      true
+    }
+
+    fn attempt_next(&mut self) -> Option<Vec<Mark>> {
+      let mut v = Vec::with_capacity(self.board.size);
+      for dist in 0..self.board.size {
+        let rr = self.sr + dist*self.dr;
+        let cc = self.sc + dist*self.dc;
+        if let Some(val) = self.board.get(rr, cc) {
+          v.push(val.clone()); // @TODO: rm clone()
+        } else {
+          return None;
+        }
+      }
+      v
+    }
+  }
+
+  impl<'a> Iterator<Vec<Mark>> for SliceIter<'a> {
+    fn next(&mut self) -> Option<Vec<Mark>> {
+      loop {
+        let has_more = self.advance();
+        if !has_more { return None; }
+        if let Some(slice) = self.attempt_next {
+          return slice;
+        }
+      }
+    }
+  }
+
   impl Board {
     pub fn new(size: usize) -> Board {
       let mut vals = Vec::with_capacity(size);
@@ -44,36 +110,26 @@ pub mod ttt {
       }
     }
 
-    pub fn winner(&self) -> Option<Mark> {
-      for sr in 0..self.size {
-        for sc in 0..self.size {
-          if let Some(target) = self.get(sr, sc) {
-            match target {
-              Mark::Empty => (),
-              Mark::X | Mark::O => {
-                for dr in -1..2i32 {
-                  for dc in -1..2i32 {
-                    for dist in 0..self.size {
-                      let rr = sr + dist*(dr as usize);
-                      let cc = sc + dist*(dc as usize);
-                      if let Some(val) = self.get(rr, cc) {
-                        if val != target {
+    pub fn slices(&'a self) -> SliceIter<'a> {
+      SliceIter::new(&self)
+    }
 
-                        }
-                        // @TODO
-                        // ugh I don't really like this func sig anyway...
-                        // seems like this should be split into a slice iterator
-                        // and a function that tells if a slice is a winner
-                      }
-                    }
-                  }
-                }
+    pub fn winner(&self) -> Option<Mark> {
+      for slice in self.slices {
+        match slice.get(0) {
+          Some(target) if target != Mark::Empty => {
+            for elem in slice.iter() {
+              if elem != target {
+                continue;
               }
             }
-          }
+            return target;
+          },
+          Some(Mark::Empty) => { continue; },
+          None => { continue; },
         }
       }
-      None // @TODO change
+      None
     }
   }
 }
